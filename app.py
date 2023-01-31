bf6b32fd984f9cdeefee3c66e8ef1ea9ebe771d0
Amysoj J A Jithisha <amysojjalouis@gmail.com>
2023-01-31 19:01:56 +0530

    updated readme.md

--- app.py
+++ app.py
@@ -1,53 +1,92 @@
+# Importing necessary modules
 from flask import Flask, jsonify, request, abort, Response
 from flask_cors import CORS, cross_origin
 import json
 
+# Initializing Flask app
 app = Flask(__name__)
 
-#####################################################################
-# mainurl = 'http://localhost:5000'
-# CORS(app)
+# URL to check against during CORS check
+mainurl = 'http://localhost:5000'
+
+# Enabling Cross Origin Resource Sharing (CORS)
+CORS(app)
+
+# Function to check for allowed origins
 
 
-# @app.before_request
-# def check():
-#     if request.origin != mainurl:
-#         error_message = json.dumps({'Data': 'Sorry this site is not supported'})
-#         abort(Response(error_message, 401))
-######################################################################
+@app.before_request
+def check():
+    # Check if request origin is not equal to mainurl
+    if request.origin != mainurl:
+        # Prepare error message as JSON
+        error_message = json.dumps(
+            {'Data': 'Sorry this site is not supported'})
+        # Abort the request with 401 status and error message
+        abort(Response(error_message, 401))
+
+# Route to handle GET requests
+
 
 @app.route('/getlocs', methods=['GET'])
 def getreq():
+    # Open data.json file in read mode
     with open('data.json', 'r') as file:
+        # Load data from the file
         file_data = json.load(file)
+    # Return file data as JSON
     return jsonify(file_data)
+
+# Route to handle POST requests
 
 
 @app.route('/postlocs', methods=['POST'])
 def postreq():
+    # Open data.json file in read-write mode
     with open('data.json', 'r+') as file:
+        # Load data from the file
         file_data = json.load(file)
+        # If request contains JSON data
         if request.json is not None:
+            # Get JSON data from the request
             json_str = request.json
+            # Append the new data to file data
             file_data[0]["data"].append(json_str)
+            # Move file pointer to the start of the file
             file.seek(0)
+            # Dump file data to the file
             json.dump(file_data, file, indent=4)
+            # Print the new data
             print(json_str)
+    # Return file data as JSON
     return jsonify(file_data)
+
+# Route to handle DELETE requests
 
 
 @app.route('/dellocs', methods=['DELETE'])
 def delreq():
+    # Open data.json file in read-write mode
     with open('data.json', 'r+') as file:
+        # Load data from the file
         file_data = json.load(file)
+        # If request contains JSON data
         if request.json is not None:
+            # Get JSON data from the request
             json_str = request.json
+            # Remove the data from file data
             file_data[0]["data"].remove(json_str)
+            # Move file pointer to the start of the file
             file.seek(0)
+            # Clear the contents of the file
             open("data.json", "w").close()
+            # Dump file data to the file
             json.dump(file_data, file, indent=4)
+    # Return file data as JSON
     return jsonify(file_data)
 
 
+# Entry point for the program
 if __name__ == "__main__":
+    # Start the code
     app.run()
--- readme.md
+++ readme.md
@@ -1,5 +1,19 @@
-# CRD API #
+# CRD API with CORS support
 
-Hi, This Flask Api has Create, Read and Delete functionality. It will manupulate data in data.json file. It is useful for frontdevs to store data in json format and retrive for testing.
+This code implements a Flask RESTful API with Cross-Origin Resource Sharing (CORS) support. The API performs CRUD operations on a local JSON file data.json. The API has the following routes:
 
-Hope you have a good day, Happy Coding :)
+## RESTful API Routes
+
+`/getlocs`: Route to handle GET requests and returns the contents of the data.json file as a JSON object.
+
+`/postlocs`: Route to handle POST requests and adds the data from the request body to the data.json file.
+
+`/dellocs`: Route to handle DELETE requests and removes the data from the request body from the data.json file.
+
+## CORS Support
+
+CORS support is implemented using the flask-cors library. The CORS class from the flask_cors module is initialized with the app object. The check function is defined to check the origin of the request. If the origin of the request is not equal to http://localhost:5000, the request is aborted with a 401 status code and an error message.
+
+## Conclusion
+
+This code provides a simple implementation of a Flask RESTful API with CORS support. The API performs basic CRUD operations on a local JSON file, and the CORS support ensures that only requests from the specified origin are processed. The code can be easily extended to perform more complex operations or to connect to a database instead of a local file.
